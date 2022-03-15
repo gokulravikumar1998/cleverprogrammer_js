c5c361dc5131de4ff4b57870eefb312ddd7f269a
gokulravikumar1998 <gokulit1620@gmail.com>
2022-03-15 12:12:20 +0530

    Guesser-2

--- Guesser/script.js
+++ Guesser/script.js
@@ -1,8 +1,8 @@
 /**
  * Guess The Number Game
  * DONE: Get user value from input and save it to variable numberGuess
- * TODO: Generate a random number 1 to 100 and save it to variable correctNumber
- * TODO: Console whether the guess is too high, too low, or is correct inside playGame function
+ * DONE: Generate a random number 1 to 100 and save it to variable correctNumber
+ * DONE: Console whether the guess is too high, too low, or is correct inside playGame function
  * TODO: Create a function called displayResult to move the logic for if the guess is too high, too low, or correct
  * TODO: Complete the showYouWon, showNumberAbove, showNumberBelow
  * TODO: Use the showYouWon... functions within displayResult to display the correct dialog
@@ -15,6 +15,7 @@
 
 // Variable for store the correct random number 
 
+let correctNumber = getRandomNumber()
 
 window.onload = function() {
     document.getElementById("number-submit").addEventListener("click", playGame);
@@ -26,7 +27,14 @@ window.onload = function() {
  */
 function playGame(){
     let guessNum = document.getElementById('number-guess').value;
-    console.log(guessNum)
+    if (guessNum > correctNumber){
+        console.log('high')
+    } else if  (guessNum < correctNumber){
+        console.log('low')
+    } else {
+        console.log('correct')
+    }
+
   // *CODE GOES BELOW HERE *
 }
 
@@ -61,6 +69,9 @@ function resetResultContent(){
  */
 function getRandomNumber(){
   // *CODE GOES BELOW HERE *
+  let correctNumber = Math.floor(Math.random() * (100 - 1 + 1) + 1)
+  return correctNumber
+
 }
 
 /**
