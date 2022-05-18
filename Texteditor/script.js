39843bff19d078fd1eeb41f3b03a8b36232ecfa7
gokulravikumar1998 <gokulit1620@gmail.com>
2022-05-18 12:44:30 +0530

    text editor

--- Texteditor/script.js
+++ Texteditor/script.js
@@ -3,7 +3,7 @@
 /**
  * Update the output text as you type in the textarea
  */
- function updateText(){
+updateText = () => {
     let text = document.getElementById("text-input").value;
     document.getElementById('text-output').innerText = text;
   }
@@ -11,7 +11,7 @@
   /**
    * Toggle the bold class for the output text
    */
-  function makeBold(elem){
+makeBold = (elem) => {
     elem.classList.toggle('active');
     document.getElementById('text-output').classList.toggle('bold');
   }
@@ -19,7 +19,7 @@
   /**
    * Toggle the italic class for the output text
    */
-  function makeItalic(elem){
+makeItalic = (elem) => {
     elem.classList.toggle('active');
     document.getElementById('text-output').classList.toggle('italic');
   }
@@ -28,25 +28,28 @@
    * Toggle the underline class for the output text
    * use contains ,romve , and add functions
    */
-  function makeUnderline(elem){
-      elem.classList.toggle('active');
-      let output = document.getElementById('text-output');
-      if(output.classList.contains('underline')){
-        output.classList.remove('underline');
-      } else {
-        output.classList.add('underline');
-      }
-  }
-  
+makeUnderline = (elem) => {
+    elem.classList.toggle('active');
+    let output = document.getElementById('text-output');
+    if(output.classList.contains('underline')){
+      output.classList.remove('underline');
+    } else {
+      output.classList.add('underline');
+    }
+}
+
   /**
    * Toggle the style textAlign attribute
    * Toggle the active state for the align butttons
    */
-  function alignText(elem, alignType){
-    document.getElementById('text-output').style.textAlign  = alignType;
-    let alignButtons = document.getElementsByClassName('align');
-    for(let i = 0; i < alignButtons.length; i++ ){
-       alignButtons[i].classList.remove('active');
-    }
-    elem.classList.toggle('active');
+alignText = (elem, alignType) => {
+  document.getElementById('text-output').style.textAlign  = alignType;
+  let alignButtons = document.getElementsByClassName('align');
+  for(k of alignButtons){
+    k.classList.remove('active')
   }
+  // alignButtons = alignButtons.map((button)=>{
+  //   return button.classList.remove('active')
+  // }) -------> this wont work on html elements (actully html elements are not array )
+  elem.classList.toggle('active');
+}
